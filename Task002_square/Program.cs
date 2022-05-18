491f66f1dd4b7c32424f15dda406ffcfb8e086b5
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-18 23:27:22 +0300

    Task002 square of number

---
+++ Task002_square/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Input the first number and press Enter: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Input the second number and press Enter: ");
+int b = int.Parse(Console.ReadLine());
+if(a*a == b) 
+    Console.WriteLine($"{b} is the square of a number {a}");
+else if(b*b == a) 
+    Console.WriteLine($"{a} is the square of a number {b}");
+else 
+    Console.WriteLine($"{a} isn't the square of a number {b} and {b} isn't the square of a number {a}.");
+
