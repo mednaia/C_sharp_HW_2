b8cb8d1231f9b475f0ca494c1e066dd4aba365bc
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-18 23:50:10 +0300

    task 003 coordinates

---
+++ Task003_coordinate/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Input number of the quarter of coordinate system and press Enter: ");
+int x = int.Parse(Console.ReadLine());
+
+if(x == 1)
+    Console.WriteLine("X>0 & Y>0");
+else if(x == 2)
+    Console.WriteLine("X<0 & Y>0");
+else if(x == 3)
+    Console.WriteLine("X<0 & Y<0");
+else if(x == 4)
+    Console.WriteLine("X>0 & Y<0");
+else
+    Console.WriteLine($"Incorrect value.");
