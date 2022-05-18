79e074cd22257b43d5ee02fd31f78c6466dff2a9
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-18 23:11:08 +0300

    task001 weekend

---
+++ Task001_weekend/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Input the number of the weekday add press Enter: ");
+int num = int.Parse(Console.ReadLine());
+
+if(num >= 1 && num <= 5) Console.WriteLine("It's not  day off.");
+if(num > 5 && num < 8) Console.WriteLine("It's weekend!");
+if(num > 7) Console.WriteLine("Incorrect number");
